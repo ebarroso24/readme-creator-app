e043df6d7811246a77c03e67d83ed63ce9b15584
ebarroso24 <134114010+ebarroso24@users.noreply.github.com>
2023-11-05 12:18:13 -0500

    included needed packages

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,23 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require("path");
+const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((responses) => {
+        console.log("Creating Professional README.md File...");
+        writeToFile("./dist/README.md", generateMarkdown({ ...responses }));
+      });
+}
 
 // Function call to initialize app
 init();
